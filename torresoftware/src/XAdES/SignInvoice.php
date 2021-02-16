0321e69501923fff0b9f864d0600e9d1a40bdaa2
Alexander Obando <gerencia@torresoftware.com>
2021-02-16 17:44:24 -0500

    Se devuelven cambios de namespaces

--- torresoftware/src/XAdES/SignInvoice.php
+++ torresoftware/src/XAdES/SignInvoice.php
@@ -112,7 +112,7 @@ class SignInvoice extends Sign
         'xmlns:cac' => 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2',
         'xmlns:ext' => 'urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2',
         'xmlns:cbc' => 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2',
-        'xmlns:sts' => 'dian:gov:co:facturaelectronica:Structures-2-1',
+        'xmlns:sts' => 'http://www.dian.gov.co/contratos/facturaelectronica/v1/Structures',
         'xmlns' => 'urn:oasis:names:specification:ubl:schema:xsd:Invoice-2',
         'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
         'xmlns:xades141' => 'http://uri.etsi.org/01903/v1.4.1#',
